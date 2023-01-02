81988fb69fa69015d6413e87d20b1978d5c42320
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-01-02 16:56:59 +0100

    Initial commit

---
+++ app/src/test/java/ynov/projet/b3tempogoncalves/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ynov.projet.b3tempogoncalves;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
